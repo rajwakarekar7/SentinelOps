b2dafe6548de5ead263c75ca15991d316269d277
Raj Wakarekar <rajwakarekar7792@gmail.com>
2026-07-11 23:34:18 +0530

    updating logger.py

--- logger.py
+++ logger.py
@@ -1,20 +1,19 @@
 from datetime import datetime
 import os
 
-LOG_FILE = os.path.join(
+BASE_DIR = os.path.dirname(__file__)
 
-    os.path.dirname(__file__),
+LOG_DIR = os.path.join(BASE_DIR, "logs")
 
-    "logs",
-    "log.txt"
-)
+LOG_FILE = os.path.join(LOG_DIR, "log.txt")
+
 
 def log(message):
 
-    file = open( LOG_FILE , "a")
+    os.makedirs(LOG_DIR, exist_ok=True)
 
     current_time = datetime.now().strftime("%H:%M:%S")
 
-    file.write(f"[{current_time}] {message}\n")
+    with open(LOG_FILE, "a") as file:
 
-    file.close()
+        file.write(f"[{current_time}] {message}\n")
